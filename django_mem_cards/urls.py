854303549066b736d3abce12ded2d542c21e87a1
Egor Korotaev <ego_kor@mail.ru>
2024-09-20 11:26:43 +0300

    Change header

--- django_mem_cards/urls.py
+++ django_mem_cards/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('homepage.urls')),
+    path('', include('homepage.urls', namespace='homepage')),
     path('admin/', admin.site.urls),
 ]
