d6de503c0dac16b075bcb9872b62bca7eaedd510
Egor Korotaev <ego_kor@mail.ru>
2024-09-21 12:21:13 +0300

    Add basic deck list to index.html

--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 
 
+placeholder_decks = [
+    {'name': 'English',
+     'cards_qty': 120,
+     'winrate': 0.78},
+
+    {'name': 'French',
+     'cards_qty': 156,
+     'winrate': 0.63},
+]
+
 def index(request):
-    template_name = 'homepage/index.html'
-    return render(request, template_name)
+    return render(
+        request,
+        template_name='homepage/index.html',
+        context={'decks': placeholder_decks})
