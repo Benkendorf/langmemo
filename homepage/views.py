803d02dc23c1d9dc7fb53ea4d570ad7fef8bf0f7
Egor Korotaev <ego_kor@mail.ru>
2024-09-21 23:13:17 +0300

    Test foreign symbols in deck names

--- homepage/views.py
+++ homepage/views.py
@@ -6,9 +6,13 @@ placeholder_decks = [
      'cards_qty': 120,
      'winrate': 0.78},
 
-    {'name': 'French',
+    {'name': '日本語',
+     'cards_qty': 243,
+     'winrate': 0.64},
+
+    {'name': 'Français',
      'cards_qty': 156,
-     'winrate': 0.63},
+     'winrate': 0.43},
 ]
 
 def index(request):
