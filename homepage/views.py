c97f3a1a267ebbe658532702be36f7b6378afebb
Egor Korotaev <ego_kor@mail.ru>
2024-10-08 18:04:13 +0300

    Modify homepage look, add "Add deck" card

--- homepage/views.py
+++ homepage/views.py
@@ -4,16 +4,25 @@ from django.shortcuts import render
 placeholder_decks = [
     {'name': 'English',
      'cards_qty': 120,
-     'winrate': 0.78},
+     'winrate': 0.78,
+     'in_queue': 28},
 
     {'name': '日本語',
      'cards_qty': 243,
-     'winrate': 0.64},
+     'winrate': 0.64,
+     'in_queue': 0},
 
     {'name': 'Français',
      'cards_qty': 156,
-     'winrate': 0.43},
+     'winrate': 0.43,
+     'in_queue': 45},
+
+    {'name': 'Deutsch',
+     'cards_qty': 349,
+     'winrate': 0.69,
+     'in_queue': 32},
 ]
+
 
 def index(request):
     return render(
