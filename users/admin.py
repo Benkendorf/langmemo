b23ae189c5a82501234cef387ff7122cef0b43bf
Egor Korotaev <ego_kor@mail.ru>
2024-11-01 19:26:40 +0300

    Add models, slightly change index.html

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import CustomUser
+from .models import CustomUser, Deck, Card
 
 admin.site.register(CustomUser, UserAdmin)
+admin.site.register(Deck)
+admin.site.register(Card)
