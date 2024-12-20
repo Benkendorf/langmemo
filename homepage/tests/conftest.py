348356fa9b5b05bdd9e34537c3e9c97c0e2288ab
Egor Korotaev <ego_kor@mail.ru>
2024-12-20 20:55:38 +0300

    Add test_deck_is_on_homepage

--- homepage/tests/conftest.py
+++ homepage/tests/conftest.py
@@ -1,9 +1,12 @@
 import pytest
 
+from datetime import timedelta
+
 from django.contrib.auth.hashers import make_password
 from django.test.client import Client
+from django.utils import timezone
 
-from deck.models import Deck
+from deck.models import Card, Deck
 
 
 @pytest.fixture
@@ -44,3 +47,36 @@ def deck(deck_owner):
 @pytest.fixture
 def deck_id_for_args(deck):
     return (deck.id, )
+
+
+@pytest.fixture
+def cards(deck):
+    all_cards = [
+        Card.objects.create(                           # NOT IN QUEUE
+            deck=deck,
+            question='first_test_question',
+            answer_1='first_test_answer_1',
+            answer_2='first_test_answer_2',
+            answer_3='first_test_answer_3',
+            right_guesses=3,
+            wrong_guesses=1,
+            datetime_created=timezone.now() - timedelta(days=15),
+            datetime_reviewed=timezone.now() - timedelta(hours=1),
+            srs_level=0,
+            srs_xp=0
+        ),
+        Card.objects.create(                           # IN QUEUE
+            deck=deck,
+            question='second_test_question',
+            answer_1='second_test_answer_1',
+            answer_2='second_test_answer_2',
+            answer_3='second_test_answer_3',
+            right_guesses=1,
+            wrong_guesses=2,
+            datetime_created=timezone.now() - timedelta(days=15),
+            datetime_reviewed=timezone.now() - timedelta(hours=6, minutes=1),
+            srs_level=0,
+            srs_xp=0
+        )
+    ]
+    return all_cards
---
+++ homepage/tests/test_content.py
@@ -0,0 +1,22 @@
+# Тут тесты, что на главную выводятся нужные колоды.
+
+from django.urls import reverse
+
+from deck.models import Card
+
+
+def test_deck_is_on_homepage(deck_owner_client, cards):
+    response = deck_owner_client.get(reverse('homepage:index'))
+    print(response.context['object_list'][0].cards_in_queue)
+    page_obj = response.context['object_list']
+    deck_count = page_obj.count()
+    card_count = page_obj[0].card_count
+    winrate = page_obj[0].winrate
+    cards_in_queue = page_obj[0].cards_in_queue
+    assert deck_count == 1
+    assert card_count == 2
+    assert winrate == sum(card.winrate for card in cards)/len(cards)
+    assert cards_in_queue == Card.objects.filter(
+        deck=page_obj[0],
+        in_queue=True
+    ).count()
