2626a67a4e790c0bec4f0344c55bd9d6307e6124
Egor Korotaev <ego_kor@mail.ru>
2025-01-14 09:11:30 +0300

    set i18n to ru-RU, change CustomUser model, so that email is required

--- users/models.py
+++ users/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 
 class CustomUser(AbstractUser):
-    pass
+    email = models.EmailField(('Адрес электронной почты'))  # Переопределяем без blank=True, чтобы имейл был обязательным
