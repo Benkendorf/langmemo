2f9ecb3c8c370120c9a0515a3fe5cc4445650a0a
Egor Korotaev <ego_kor@mail.ru>
2025-06-10 22:13:25 +0300

    small change

---
+++ backend/django_mem_cards/constants.py
@@ -0,0 +1,25 @@
+CARDS_PAGINATION_LIMIT = 15
+CARD_BAD_WINRATE_LIMIT = 50
+DAM_LEV_DIST_LIMIT = 1
+SRS_LEVELS = {
+    0: {'xp_to_next_lvl': 3, 'time_interval_hrs': 6},
+    1: {'xp_to_next_lvl': 5, 'time_interval_hrs': 24},
+    2: {'xp_to_next_lvl': 5, 'time_interval_hrs': 48},
+    3: {'xp_to_next_lvl': 10, 'time_interval_hrs': 72},
+    4: {'xp_to_next_lvl': None, 'time_interval_hrs': 120}
+}
+
+REVIEW_SUCCESS_MESSAGE = 'Правильно!'
+REVIEW_FAILURE_MESSAGE = 'Неправильно!'
+REVIEW_NOT_IN_QUEUE_MESSAGE = 'Упс! Карты уже нет в очереди!'
+
+DECKS_PAGINATION_LIMIT = 9
+DECK_BAD_WINRATE_LIMIT = 50
+WEEKDAYS_RUS = ['Понедельник', 'Вторник', 'Среда',
+                'Четверг', 'Пятница', 'Суббота', 'Воскресенье']
+TOTAL_CALENDAR_DAYS = 5
+
+API_TOKEN_LENGTH = 30
+TG_USER_ID_MAX_LENGTH = 50
+TG_BOT_USERNAME = '<PLACEHOLDER>'
+
