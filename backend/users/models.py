e33df9004d5d571d5208b4540eb981b3b6180aa2
Egor Korotaev <ego_kor@mail.ru>
2025-06-10 21:43:31 +0300

    change user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from django_mem_cards.constants import API_TOKEN_LENGTH, TG_USER_ID_MAX_LENGTH
+
 
 class CustomUser(AbstractUser):
     email = models.EmailField(('Адрес электронной почты'))  # Переопределяем без blank=True, чтобы имейл был обязательным
     api_token = models.CharField(
-        max_length=40,
+        max_length=API_TOKEN_LENGTH,
         unique=True,
         null=True,
         blank=True
     )
+    telegram_chat_id = models.CharField(max_length=TG_USER_ID_MAX_LENGTH, null=True, blank=True, unique=True)
